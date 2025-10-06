f405aa0d9af76b6441293def80792d153a7f3eed
HueHue-dev <dev@web-hl.de>
2025-10-06 16:09:24 +0200

    env variable naming and htmx variable naming improvements

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ type PageData struct {
 	Error          *string
 }
 
+var htmxTemplateName = "response"
+
 func main() {
 	redisClient := utils.RedisClient()
 	if redisClient == nil {
@@ -30,7 +32,7 @@ func main() {
 
 	http.HandleFunc("/", func(writer http.ResponseWriter, req *http.Request) {
 		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		data := PageData{Title: os.Getenv("TITLE")}
+		data := PageData{Title: os.Getenv("APP_TITLE")}
 		err := tmpl.Execute(writer, data)
 		if err != nil {
 			return
@@ -43,8 +45,8 @@ func main() {
 		url := req.FormValue("url")
 		if url == "" {
 			errorMessage := "The URL field cannot be empty."
-			data := PageData{Title: os.Getenv("TITLE"), Error: &errorMessage}
-			if err := tmpl.ExecuteTemplate(writer, "result", data); err != nil {
+			data := PageData{Title: os.Getenv("APP_TITLE"), Error: &errorMessage}
+			if err := tmpl.ExecuteTemplate(writer, htmxTemplateName, data); err != nil {
 				http.Error(writer, "Failed to render template", http.StatusInternalServerError)
 			}
 			return
@@ -56,8 +58,8 @@ func main() {
 		expirationDays, err := strconv.Atoi(expirationStr)
 		if err != nil || expirationDays < 1 {
 			errorMessage := "Invalid expiration value."
-			data := PageData{Title: os.Getenv("TITLE"), Error: &errorMessage}
-			if err := tmpl.ExecuteTemplate(writer, "result", data); err != nil {
+			data := PageData{Title: os.Getenv("APP_TITLE"), Error: &errorMessage}
+			if err := tmpl.ExecuteTemplate(writer, htmxTemplateName, data); err != nil {
 				http.Error(writer, "Failed to render template", http.StatusInternalServerError)
 			}
 
@@ -70,8 +72,8 @@ func main() {
 		if alias != "" {
 			if _, err := utils.GetLongURL(&ctx, redisClient, alias); err == nil {
 				errorMessage := "Alias already taken."
-				data := PageData{Title: os.Getenv("TITLE"), Error: &errorMessage}
-				if err := tmpl.ExecuteTemplate(writer, "result", data); err != nil {
+				data := PageData{Title: os.Getenv("APP_TITLE"), Error: &errorMessage}
+				if err := tmpl.ExecuteTemplate(writer, htmxTemplateName, data); err != nil {
 					http.Error(writer, "Failed to render template", http.StatusInternalServerError)
 				}
 
@@ -81,14 +83,14 @@ func main() {
 			shortURL = utils.GetShortCode()
 		}
 
-		fullShortURL := fmt.Sprintf(os.Getenv("URL")+"r/%s", shortURL)
+		fullShortURL := fmt.Sprintf(os.Getenv("APP_URI")+"r/%s", shortURL)
 
 		utils.SetKey(&ctx, redisClient, shortURL, url, expiration)
 
 		expirationTime := time.Now().Add(expiration).Format("2006-01-02 15:04")
-		data := PageData{Title: os.Getenv("TITLE"), ShortenedURL: &fullShortURL, ExpirationDate: &expirationTime}
+		data := PageData{Title: os.Getenv("APP_TITLE"), ShortenedURL: &fullShortURL, ExpirationDate: &expirationTime}
 
-		if err := tmpl.ExecuteTemplate(writer, "result", data); err != nil {
+		if err := tmpl.ExecuteTemplate(writer, htmxTemplateName, data); err != nil {
 			http.Error(writer, "Failed to render template", http.StatusInternalServerError)
 		}
 
